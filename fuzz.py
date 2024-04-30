5a730d99cd75e9482b4dde6d27f6f3e59be46b3b
hpressnell <hamiltonpressnell@gmail.com>
2024-04-30 12:19:16 -0500

    Finished Fuzzer

--- fuzz.py
+++ fuzz.py
@@ -13,30 +13,30 @@ def simple_fuzz():
     test_list = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
     [10,10,10,10,10,10,10],
     [1.0, 1, 1, 2.5])
-    try:
-        for x in test_list:
-            print(Average(x))
-            print(Median(x))
-            print(giveTimeStamp())
-        # print(getFileLength("report.py"))
-        os.chdir("..")
-        # print(os.getcwd())
-        new_directory_path = os.path.join(os.getcwd(), 'FAME-ML')
-        os.chdir(new_directory_path)
-        sys.path.append(os.getcwd())
-        from main import getAllPythonFilesinRepo
-        print(getAllPythonFilesinRepo(os.getcwd()))
+    # try:
+    for x in test_list:
+        print(Average(x))
+        print(Median(x))
+        print(giveTimeStamp())
+    # print(getFileLength("report.py"))
+    os.chdir("..")
+    # print(os.getcwd())
+    new_directory_path = os.path.join(os.getcwd(), 'FAME-ML')
+    os.chdir(new_directory_path)
+    sys.path.append(os.getcwd())
+    from main import getAllPythonFilesinRepo
+    print(getAllPythonFilesinRepo(os.getcwd()))
 
 
-        os.chdir("..")
-        # print(os.getcwd())
-        new_directory_path = os.path.join(os.getcwd(), 'mining')
-        os.chdir(new_directory_path)
-        sys.path.append(os.getcwd())
-        from mining import checkPythonFile
-        print(checkPythonFile(os.getcwd()))
-    except Exception as e:
-        print(e)
+    os.chdir("..")
+    # print(os.getcwd())
+    new_directory_path = os.path.join(os.getcwd(), 'mining')
+    os.chdir(new_directory_path)
+    sys.path.append(os.getcwd())
+    from mining import checkPythonFile
+    print(checkPythonFile(os.getcwd()))
+    # except Exception as e:
+    #     print(e)
 
 if __name__ == '__main__':
     simple_fuzz()
